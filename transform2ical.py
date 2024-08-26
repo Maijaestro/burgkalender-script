d38c123a9842f1e477824eb9ffe49f1cbacef0e2
Benedikt Wallbrecht <benedikt.wallbrecht@mandato.eu>
2024-08-26 13:56:28 +0200

    Initial Commit

---
+++ transform2ical.py
@@ -0,0 +1,33 @@
+from ics import Calendar, Event
+from datetime import datetime, timedelta
+import json, pytz
+
+# JSON-Datei laden
+with open("events.json", "r", encoding="utf-8") as f:
+    events_data = json.load(f)
+
+# Kalender erstellen
+calendar = Calendar()
+
+# Zeitzone setzen
+tz = pytz.timezone("Europe/Berlin")
+
+# Ereignisse hinzufügen
+for date, event in events_data.items():
+    e = Event()
+    e.name = event["event_artist"]
+
+    start_time = datetime.strptime(event["event_starttime"], "%Y-%m-%d %H:%M")
+    start_time = tz.localize(start_time)
+    e.begin = start_time
+    e.duration = timedelta(hours=2)
+
+    e.location = event["event_location"]
+
+    e.url = event["event_info"]
+
+    calendar.events.add(e)
+
+# iCal-Datei speichern
+with open("events.ics", "w") as f:
+    f.writelines(calendar)
